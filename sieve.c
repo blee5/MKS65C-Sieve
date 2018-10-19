afb198c86a5c7c3a78248b5c36e267844af9614f
Brian <hlee5@stuy.edu>
2018-10-18 20:55:11 -0400

    Wait I can't just sqrt the indices

--- sieve.c
+++ sieve.c
@@ -17,7 +17,7 @@ int sieve(int n)
     int i = 2; n--;
     int j;
     int num;
-    for (; i < sqrt(len); i++, p++)
+    for (; i < len; i++, p++)
     {
         if (*p == 0)
         {
@@ -38,12 +38,14 @@ int sieve(int n)
             }    
         }
     }
-    for (; i < len; i++, p++)
-    {
-        if (*p == 0 &&--n <= 0)
-        {
-            return (i >> 1) - 1;
-        }
-    }
+    /*
+     * for (; i < len; i++, p++)
+     * {
+     *     if (*p == 0 &&--n <= 0)
+     *     {
+     *         return (i >> 1) - 1;
+     *     }
+     * }
+     */
     return -1;
 }
