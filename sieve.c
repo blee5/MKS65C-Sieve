83f01428650b6fd5f8f36075a6016c075db896d9
Brian Lee <hlee5@stuy.edu>
2018-10-18 20:27:52 -0400

    Experiment

--- sieve.c
+++ sieve.c
@@ -4,27 +4,39 @@
 
 int sieve(int n)
 {
+    if (n == 1)
+    {
+        return 2;
+    }
     int len = (int)(n * log(n) * 1.15 + 50);
 
     char *sieve = calloc(sizeof(char), len);
     char *p = sieve + 2;
     char *q;
 
-    int i = 2;
-    int j = 0;
+    int i = 2; n--;
+    int j;
+    int temp;
     for (; i < len; i++, p++)
     {
+        num = (i << 1) - 1; // Index i represents (2i - 1), so we only store odd numbers
+        // printf("[%d]: %d holds %x\n", i, num, *p);
         if (*p == 0)
         {
             if (--n <= 0)
             {
-                return i;
+                return num;
             }
             j = i;
-            for (q = p; j < len; j += i, q += i)
+            // Increment j by num, then j represents the number
+            // 2 (j + num - 1)
+            // => 2j + 2num - 2
+            // => 2(j-1) + 2num, which is the next odd multiple of num
+            for (q = p; j < len; j += num, q += temp)
             {
                 if (*q == 0)
                 {
+                    // printf("[%d]: %d set to 0\n", j, (j << 1) - 1); 
                     *q = 1;
                 }
             }    
